ac645d052502955f5e5f105e95545792e0ad38ce
Curiouss <wanghaoqi1124@gmail.com>
2016-03-22 20:23:20 -0500

    191 AC

---
+++ Number_of_1bits.py
@@ -0,0 +1,18 @@
+
+n = 5
+N = bin(n)
+N = str(N)
+L = len(N)-1
+count = 0
+def Weight(L):
+    if L == 1:
+        return count
+    elif L == 0:
+        return 0
+    else:
+        if N[L]=='1':
+            return 1 + Weight(L-1)
+        else:
+            return Weight(L-1)
+
+Weight(L)
