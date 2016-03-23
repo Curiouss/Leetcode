d8e4ddee098f60aed2807e9bfa241bb749d22118
Curiouss <wanghaoqi1124@gmail.com>
2016-03-23 16:39:11 -0500

    283 F

--- move_zeros.py
+++ move_zeros.py
@@ -1,22 +1,21 @@
-##class Solution(object):
-##    def moveZeroes(self, nums):
-##        """
-##        :type nums: List[int]
-##        :rtype: void Do not return anything, modify nums in-place instead.
-##        """
-nums = [1,2,0,3,4,0,0,6,8,7,0,0,0,1]
-L = len(nums)-1
-i = L
-N = []
-for x in range(L,0,-1):
-    if nums[x] == 0:
-        if x == L:
-            nums = nums[0:x]
-        else:
-            numsend = nums[x+1:]
-            nums = nums[0:x]
-            nums= nums +numsend
-        N.append(i)
-    i = i + 1
-L_zero = len(N)
-nums = nums + [0]*L_zero
+def moveZeroes(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    L = len(nums)-1
+    i = L
+    N = []
+    for x in range(L,0,-1):
+        if nums[x] == 0:
+            if x == L:
+                nums = nums[0:x]
+            else:
+                numsend = nums[x+1:]
+                nums = nums[0:x]
+                nums= nums +numsend
+            N.append(i)
+        i = i + 1
+    L_zero = len(N)
+    nums = nums + [0]*L_zero
+    return nums
