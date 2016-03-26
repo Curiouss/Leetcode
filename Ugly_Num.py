699c6e806c273e76e43374cbb7b3b5a6f5e188d7
Curiouss <wanghaoqi1124@gmail.com>
2016-03-25 20:56:16 -0500

    263 AC

--- Ugly_Num.py
+++ Ugly_Num.py
@@ -3,20 +3,9 @@ def isUgly(self, num):
     :type num: int
     :rtype: bool
     """
-    N = True
-    while N == True:
-        if num/2 == num//2:
-            num = num//2
-        else:
-            N = False
-    while N == False:
-        if num/3 == num//3:
-            num = num//3
-        else:
-            N = True
-    while N == True:
-        if num/5 == num//5:
-            num = num//5
-        else:
-            N = False
+    if num <= 0:
+        return False
+    for x in [2, 3, 5]:
+        while num % x == 0:
+            num /= x
     return num == 1
