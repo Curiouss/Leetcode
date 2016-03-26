d5d121e9978beae3e251e419079e79d0bbf1d274
Curiouss <wanghaoqi1124@gmail.com>
2016-03-25 23:27:30 -0500

    Gomoku-Part1

--- Judge.py
+++ Judge.py
@@ -1,5 +1,8 @@
 board= [ [ 0 for i in range(25) ] for j in range(25) ]
-
+board[9][9]=1
+board[8][8]=1
+board[7][7]=1
+board[6][6]=1
 
 def Judge(board,n):
     # board is a 25x25 list
@@ -7,21 +10,23 @@ def Judge(board,n):
     # n is a 1x2 list [x,y]
     [x,y]=n
     board[x][y]=1 #define this point to 1 to run the function
-    counter = 0
+    counter = -0.5
     Judg = [ 0 for i in range(4) ]
     EightArray = [[1,1],[-1,-1],[1,0],[-1,0],[-1,1],[1,-1],[0,1],[0,-1]]
     for Arraynumber in EightArray:
         [xA,xB]= Arraynumber
-        print([xA,xB])
         num=1  # connected numbers
         Check = 1
         while Check==True:
             if board[x][y]==board[x+xA*num][x+xB*num]:
                 num = num + 1
+                if num >4:
+                    Check = False
             else:
                 num = num -1
             Check = False
         counter = counter + 0.5
+        print(counter)
         Judg[int(counter//1)]=Judg[int(counter//1)]+num
     return set(Judg) & {4} == {4}
 
