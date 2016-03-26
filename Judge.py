55c40d7b295ecfa0a70167f1cfa863cc4b46159f
Curiouss <wanghaoqi1124@gmail.com>
2016-03-25 23:40:22 -0500

    Gomoku-Part1

--- Judge.py
+++ Judge.py
@@ -2,13 +2,14 @@ board= [ [ 0 for i in range(25) ] for j in range(25) ]
 board[9][9]=1
 board[8][8]=1
 board[7][7]=1
-board[6][6]=1
+board[11][11]=1
 
 def Judge(board,n):
     # board is a 25x25 list
     # player is a int 1 or 2
     # n is a 1x2 list [x,y]
     [x,y]=n
+##    print(x,y)
     board[x][y]=1 #define this point to 1 to run the function
     counter = -0.5
     Judg = [ 0 for i in range(4) ]
@@ -18,16 +19,19 @@ def Judge(board,n):
         num=1  # connected numbers
         Check = 1
         while Check==True:
-            if board[x][y]==board[x+xA*num][x+xB*num]:
-                num = num + 1
+##            print([x+xA*num],[y+xB*num])
+            if board[x][y]==board[x+xA*num][y+xB*num]:
+##                print(num)
                 if num >4:
                     Check = False
+                num = num + 1
             else:
                 num = num -1
-            Check = False
+                Check = False
         counter = counter + 0.5
-        print(counter)
+##        print(counter,num)
         Judg[int(counter//1)]=Judg[int(counter//1)]+num
+##        print(set(Judg))
     return set(Judg) & {4} == {4}
 
 
