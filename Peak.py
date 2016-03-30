fa1162ca9b462d0e61a9fe79d10c2f34b8ab4fb9
Curiouss <wanghaoqi1124@gmail.com>
2016-03-29 21:53:49 -0500

    162

---
+++ Peak.py
@@ -0,0 +1,11 @@
+##162 Find Peak Element
+
+##class Solution(object):
+##    def findPeakElement(self, nums):
+##        """
+##        :type nums: List[int]
+##        :rtype: int
+##        """
+L = [1,2,3,4,5,6,5,6,4]
+Len = len(L)
+mid =
