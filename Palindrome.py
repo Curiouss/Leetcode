4620093ffbe14f37baeef4f3e99fa3f8a98c0425
Curiouss <wanghaoqi1124@gmail.com>
2016-03-30 21:58:46 -0500

    9

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+# 9. Palindrome Number
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         """
+#         :type x: int
+#         :rtype: bool
+#         """
+
+ x = str(x)
+
+
+
