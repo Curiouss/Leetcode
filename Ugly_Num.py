6e42cc60b4a1f016ebf1f9f0d8b77332f7d09295
Curiouss <wanghaoqi1124@gmail.com>
2016-04-01 15:51:55 -0500

    263 AC

--- Ugly_Num.py
+++ Ugly_Num.py
@@ -1,11 +1,12 @@
-def isUgly(self, num):
-    """
-    :type num: int
-    :rtype: bool
-    """
-    if num <= 0:
-        return False
-    for x in [2, 3, 5]:
-        while num % x == 0:
-            num /= x
-    return num == 1
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        for x in [2, 3, 5]:
+            while num % x == 0:
+                num /= x
+        return num == 1
