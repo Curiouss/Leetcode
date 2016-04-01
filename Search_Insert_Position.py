39bd6f5275c562bde5ac0fd4c9bbfa0a67355364
Curiouss <wanghaoqi1124@gmail.com>
2016-04-01 15:13:01 -0500

    35 AC

---
+++ Search_Insert_Position.py
@@ -0,0 +1,19 @@
+# class Solution(object):
+def searchInsert(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    num = nums
+    t = target
+    L = len(num)
+    i = 0
+    while i < L:
+        if num[i] == t:
+            return i
+        elif num[i] < t:
+            i += 1
+        else:
+            return i
+    return i
