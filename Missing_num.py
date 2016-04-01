83ff0f81798393790420c638b18cc1f1034d625d
Curiouss <wanghaoqi1124@gmail.com>
2016-04-01 16:39:01 -0500

    268 AC

--- Missing_num.py
+++ Missing_num.py
@@ -1,9 +1,11 @@
-# class Solution(object):
-
 def missingNumber(self, nums):
     """
     :type nums: List[int]
     :rtype: int
     """
-    L =len(nums)
-
+    L = len(nums)
+    S = set(range(L + 1))
+    N = set(nums)
+    NUM = S - N
+    t = list(NUM)
+    return t[0]
