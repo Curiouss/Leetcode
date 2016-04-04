ace320b31c953e8cd5cf96895ab4c415587ee1cd
Curiouss <wanghaoqi1124@gmail.com>
2016-04-03 21:18:24 -0500

    -

--- Length_Lword.py
+++ Length_Lword.py
@@ -12,3 +12,12 @@ class Solution(object):
                 l = l[:-1]
             return len(l[-1])
         
+        size = len(nums)
+        if size == 0:
+            return 0
+        dp = [0] * size
+        ans = -2147483648
+        for i in range(0, size):
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+            ans = max(ans, dp[i])
+        return ans
