b2e2ca02820fe1de8e98d10d5601dd884860102a
Curiouss <wanghaoqi1124@gmail.com>
2016-04-05 12:08:55 -0500

    125

---
+++ huiwen.py
@@ -0,0 +1,30 @@
+def isPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    length = len(s)
+    i = 0
+    j = length - 1
+    counter1 = 0
+    counter2 = 0
+    if s == "" or s.isalpha() == 0:
+        return True
+    while i + 1 < length and j > 0:
+        if s[i].isalpha() == 0:
+            i += 1
+            counter1 += 1
+        if s[j].isalpha() == 0:
+            j -= 1
+            counter1 += 1
+        if s[i].isalpha() == 1 and s[j].isalpha() == 1:
+            if s[i].upper() == s[j].upper():
+                i += 1
+                j -= 1
+            else:
+                return False
+        if counter1 == counter2:
+            i += 1
+            j -= 1
+            counter1 = counter2
+    return True
