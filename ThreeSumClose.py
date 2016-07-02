c21e1d0f350adb2d9c4226f2512f904cec285b37
Haoqi <wanghaoqi1124@gmail.com>
2016-07-02 21:50:46 +0800

    16

---
+++ ThreeSumClose.py
@@ -0,0 +1,15 @@
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        Length=len(nums)
+        Rtarget=-2**32
+        for i in range(Length-3):
+            for j in range(i,Length-2):
+                for k in range(j,Length-1):
+                    Sum = nums[i]+nums[j]+nums[k]
+                    if abs(Rtarget-target)>abs(Sum-target):
+                    Rtarget = Sum
+        return Rtarget
