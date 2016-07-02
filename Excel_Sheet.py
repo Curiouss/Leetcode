6f04004e33e1c69e3cf0c6d1cc8a9d3e73475fba
Haoqi <wanghaoqi1124@gmail.com>
2016-07-02 19:49:59 +0800

    168

--- Excel_Sheet.py
+++ Excel_Sheet.py
@@ -3,9 +3,11 @@ def convertToTitle(self, n):
     rest = n
     Excel=''
     Count=0
+
     while rest>26:
         rest=rest//26
         Count+=1
+
     for i in range(Count,-1,-1):
         Num=n//(26**i)
         Char=chr(Num+64)
